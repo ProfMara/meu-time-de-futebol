e6bcb4f39e21b17c81da761956bcb1d7df2786af
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-19 15:02:52 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,46 @@
+
+//criar a matriz
+
+//é aqui que cria a matrizHTML e a variável semVirgulas
+
+
+function enviar(){
+    //repete os comandos 5 vezes
+
+    
+    //é aqui que cria a matrizHTML com 5 linhas
+
+
+    //é aqui que tira as vírgulas
+
+    //exibe a lista sem as vírgulas
+
+
+
+    document.getElementById("enviarBotao").style.display = "none";
+    document.getElementById("organizarBotao").style.display = "inline-block";
+    document.getElementById("mostrarNomes").style.display = "block";
+  
+
+}
+var html = '';
+
+function organizar(){
+    //esconde a lista
+    document.getElementById("mostrarNomes").style.display = "none";
+    document.getElementById("mostrarNomesOrganizado").style.display = "block";
+   
+    //ordena todos os elementos da lista por ordem alfabética
+
+    
+    //limpa a matriz
+
+
+    //repete os comandos 5 vezes
+
+    //tira as vírgulas
+
+    //exibe a matriz sem vírgulas
+
+}
+
